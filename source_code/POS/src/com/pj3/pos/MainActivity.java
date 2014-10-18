d9456928154959b1cb5cf66cd26ae733d3616896
tuanict <tuancnttbk93@gmail.com>
2014-10-18 16:21:43 +0700

    Sửa lỗi resource

--- source_code/POS/src/com/pj3/pos/MainActivity.java
+++ source_code/POS/src/com/pj3/pos/MainActivity.java
@@ -1,11 +1,10 @@
 package com.pj3.pos;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +19,4 @@ public class MainActivity extends ActionBarActivity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
