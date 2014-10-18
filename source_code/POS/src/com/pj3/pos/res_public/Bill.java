28cfb83113c32405ed61bba00d408f0bdcb2540d
tuanict <tuancnttbk93@gmail.com>
2014-10-18 16:38:39 +0700

    Resource public

---
+++ source_code/POS/src/com/pj3/pos/res_public/Bill.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.pj3.pos.res_public;
+
+import java.util.Date;
+
+/**
+ * @author LêCông
+ *
+ */
+public class Bill {
+	private int b_id;
+	private int b_count;
+	private Date b_time_stamp;
+	public int getB_id() {
+		return b_id;
+	}
+	public void setB_id(int b_id) {
+		this.b_id = b_id;
+	}
+	public int getB_count() {
+		return b_count;
+	}
+	public void setB_count(int b_count) {
+		this.b_count = b_count;
+	}
+	public Date getB_time_stamp() {
+		return b_time_stamp;
+	}
+	public void setB_time_stamp(Date b_time_stamp) {
+		this.b_time_stamp = b_time_stamp;
+	}
+	
+}
--- source_code/POS/src/com/pj3/pos/sqlite/EmployeeHelper.java
+++ source_code/POS/src/com/pj3/pos/sqlite/EmployeeHelper.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * @author LÍCÙng
