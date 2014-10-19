d61b6c0ccf77ed8862c18a5277a53f3b4ce3da3c
tuanict <tuancnttbk93@gmail.com>
2014-10-19 16:07:41 +0700

    cập nhật sqlite

--- source_code/POS/src/com/pj3/pos/sqlite/DatabaseHelper.java
+++ source_code/POS/src/com/pj3/pos/sqlite/DatabaseHelper.java
@@ -91,8 +91,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			+ COLUMN_P_SALARY + "INT NOT NULL, "
 			+ "PRIMARY KEY (p_id)";
 	
-	//FoodStatis
-	public static final String DATABASE_CREATE = "create table"
+	//FoodStatis table create statement
+	public static final String DATABASE_F_CREATE = "create table"
 			+ TABLE_FOODSTATISTIC + "("
 			+ COLUMN_F_ID + "INT AUTO INCREAMENT NOT NULL, "
 			+ COLUMN_F_COUNT + "INT NOT NULL, "
@@ -103,30 +103,48 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			+ "CONSTRAINT f_b_id FOREIGN KEY (f_b_id) REFERENCES BILL(b_id), "
 			+ "CONSTRAINT f_m_id FOREIGN KEY (f_m_id) REFERENCES MENU(m_id)";
 	
+	// Bill table create statement
+	public static final String DATABASE_B_CREATE = "create table"
+			+ TABLE_BILL + "("
+			+ COLUMN_B_ID + "INT AUTO INCREMENT NOT NULL, "
+			+ COLUMN_B_COUNT + "INT NOT NULL, "
+			+ COLUMN_B_TIME_STAMP + "DATETIME NOT NULL, "
+			+ "PRIMARY KEY (b_id)";
 	
+	//MENU table create statement
+	public static final String DATABASE_M_CREATE = "create table"
+			+ COLUMN_M_ID + "INT AUTO INCREMENT NOT NULL, "
+			+ COLUMN_M_NAME + "VARCHAR(45) NOT NULL"
+			+ COLUMN_M_PRICE + "INT NOT NULL, "
+			+ COLUMN_M_IMAGE + "VARCHAR(45) NOTNULL"
+			+ COLUMN_M_STATUS + "BOOL NOT NULL";
 	
-	public DatabaseHelper(Context context, String name, CursorFactory factory,
-			int version) {
-		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
+	//Construct
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
-	/* (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
-	 */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		// creating required tables
+		db.execSQL(DATABASE_E_CREATE);
+		db.execSQL(DATABASE_P_CREATE);
+		db.execSQL(DATABASE_F_CREATE);
+		db.execSQL(DATABASE_B_CREATE);
+		db.execSQL(DATABASE_M_CREATE);
 	}
 
-	/* (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
-	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+		// on upgrade drop older tables
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_EMPLOYEE);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_POSITION);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_FOODSTATISTIC);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_BILL);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_MENU);
+		
+		// create new tables
+		onCreate(db);
 	}
 
 }
