0f7ebe40bdefadac96432d382d9388889348210d
tuanict <tuancnttbk93@gmail.com>
2014-10-20 17:18:07 +0700

    Handler data

--- source_code/POS/src/com/pj3/pos/sqlite/DatabaseSource.java
+++ source_code/POS/src/com/pj3/pos/sqlite/DatabaseSource.java
@@ -3,11 +3,14 @@
  */
 package com.pj3.pos.sqlite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.pj3.pos.res_public.Bill;
 import com.pj3.pos.res_public.Employee;
@@ -18,89 +21,273 @@ import com.pj3.pos.res_public.Menu;
  *
  */
 public class DatabaseSource implements SqliteAPIs{
-	DatabaseHelper databaseHelper;
+	DatabaseHelper dHelper;
 	
 	public DatabaseSource(Context context){
-		databaseHelper = new DatabaseHelper(context);
+		dHelper = new DatabaseHelper(context);
 	}
 	@Override
 	public int createUser(Employee user) {
-		SQLiteDatabase db = databaseHelper.getWritableDatabase();
+		//Check email exits
+		List<Employee> employees = this.getAllUsers();
+		int size = employees.size();
+		int count = 0;
+		while(count < size){
+			if (employees.get(count).getE_email().equals(user.getE_email()))
+				return -1;
+			else count++;
+		}
+		
+		SQLiteDatabase db = dHelper.getWritableDatabase();
 		ContentValues values = new ContentValues();
 		
+		values.put(dHelper.COLUMN_E_NAME,user.getE_name());
+		values.put(dHelper.COLUMN_E_EMAIL, user.getE_email());
+		values.put(dHelper.COLUMN_E_PASS, user.getE_pass());
+		values.put(dHelper.COLUMN_E_IMAGE, user.getE_image());
+		values.put(dHelper.COLUMN_E_PHONE, user.getE_phone_number());
+		values.put(dHelper.COLUMN_E_POSITION, user.getPOSITION_p_id());
 		
-		return 0;
+		//insert row
+		int userId = (int) db.insert(dHelper.TABLE_EMPLOYEE, null, values);
+		return userId;
 	}
 
 	@Override
 	public Employee getUser(int userId) {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		String query = "SELECT * FROM " + dHelper.TABLE_EMPLOYEE
+				+ "WHERE" + dHelper.COLUMN_E_ID + " = " + userId;
+		
+		Log.e(dHelper.LOG, query);
+		Cursor c = db.rawQuery(query, null);
+		
+		if(c != null)
+			c.moveToFirst();
+		
+		Employee user = new Employee();
+		user.setE_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_ID)));
+		user.setE_name(c.getString(c
+				.getColumnIndex(dHelper.COLUMN_E_NAME)));
+		user.setE_email(c.getString(c.getColumnIndex(dHelper.COLUMN_E_EMAIL)));
+		user.setE_pass(c.getString(c.getColumnIndex(dHelper.COLUMN_E_PASS)));
+		user.setE_image(c.getString(c.getColumnIndex(dHelper.COLUMN_E_IMAGE)));
+		user.setE_phone_number(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_PHONE)));
+		user.setPOSITION_p_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_POSITION)));
+		
+		return user;
 	}
 
 	@Override
 	public List<Employee> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Employee> employees = new ArrayList<Employee>();
+		String query = "SELECT * FROM " + dHelper.TABLE_EMPLOYEE;
+		Log.e(dHelper.LOG, query);
+		
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		Cursor c = db.rawQuery(query, null);
+		
+		//looping through all rows and adding to list
+		if(c.moveToFirst()){
+			do{
+				Employee user = new Employee();
+				user.setE_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_ID)));
+				user.setE_name(c.getString(c
+						.getColumnIndex(dHelper.COLUMN_E_NAME)));
+				user.setE_email(c.getString(c.getColumnIndex(dHelper.COLUMN_E_EMAIL)));
+				user.setE_pass(c.getString(c.getColumnIndex(dHelper.COLUMN_E_PASS)));
+				user.setE_image(c.getString(c.getColumnIndex(dHelper.COLUMN_E_IMAGE)));
+				user.setE_phone_number(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_PHONE)));
+				user.setPOSITION_p_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_POSITION)));
+				
+				employees.add(user);
+			}while(c.moveToNext());
+		}
+		return employees;
 	}
 
 	@Override
 	public List<Employee> getUserByPosition(int postionId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Employee> employees = new ArrayList<Employee>();
+		String query = "SELECT * FROM " + dHelper.TABLE_EMPLOYEE 
+				+ "WHERE" + dHelper.COLUMN_E_POSITION + " = " +postionId;
+		Log.e(dHelper.LOG, query);
+		
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		Cursor c = db.rawQuery(query, null);
+		
+		//looping through all rows and adding to list
+		if(c.moveToFirst()){
+			do{
+				Employee user = new Employee();
+				user.setE_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_ID)));
+				user.setE_name(c.getString(c
+						.getColumnIndex(dHelper.COLUMN_E_NAME)));
+				user.setE_email(c.getString(c.getColumnIndex(dHelper.COLUMN_E_EMAIL)));
+				user.setE_pass(c.getString(c.getColumnIndex(dHelper.COLUMN_E_PASS)));
+				user.setE_image(c.getString(c.getColumnIndex(dHelper.COLUMN_E_IMAGE)));
+				user.setE_phone_number(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_PHONE)));
+				user.setPOSITION_p_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_E_POSITION)));
+				
+				employees.add(user);
+			}while(c.moveToNext());
+		}
+		return employees;
 	}
 
 	@Override
 	public boolean updateUser(Employee user) {
-		// TODO Auto-generated method stub
-		return false;
+		SQLiteDatabase db = dHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		
+		boolean ok = false;
+		
+		values.put(dHelper.COLUMN_E_NAME,user.getE_name());
+		values.put(dHelper.COLUMN_E_EMAIL, user.getE_email());
+		values.put(dHelper.COLUMN_E_PASS, user.getE_pass());
+		values.put(dHelper.COLUMN_E_IMAGE, user.getE_image());
+		values.put(dHelper.COLUMN_E_PHONE, user.getE_phone_number());
+		values.put(dHelper.COLUMN_E_POSITION, user.getPOSITION_p_id());
+		try{
+			int result = db.update(dHelper.TABLE_EMPLOYEE, values,
+					dHelper.COLUMN_E_ID + " = ?",
+					new String[] { String.valueOf(user.getE_id()) });
+			ok = true;
+		}catch(Exception e){
+			ok = false;
+		}
+		return ok;
 	}
 
 	@Override
 	public boolean checkUser(String username, String password) {
-		// TODO Auto-generated method stub
+		List<Employee> employees = this.getAllUsers();
+		int size = employees.size();
+		int count = 0;
+		while(count < size){
+			if (employees.get(count).getE_email().equals(username)
+					&& employees.get(count).getE_pass().equals(password))
+				return true;
+			else count++;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean deleteUser(int userId) {
-		// TODO Auto-generated method stub
-		return false;
+		SQLiteDatabase db = dHelper.getWritableDatabase();
+		boolean ok = false;
+		try{
+			db.delete(dHelper.TABLE_EMPLOYEE, dHelper.COLUMN_E_ID + " = ?",
+					new String[] { String.valueOf(userId) });
+			ok = true;
+		}catch(Exception e){
+			ok = false;
+		}
+		return ok;
 	}
 
 	@Override
 	public int createFood(Menu menu) {
-		// TODO Auto-generated method stub
-		return 0;
+		List<Menu> menus = this.getAllFood();
+		int size = menus.size();
+		int count = 0;
+		while(count < size){
+			if(menus.get(count).getM_name().equals(menu.getM_name()))
+				return -1;
+		}
+		
+		SQLiteDatabase db = dHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		
+		values.put(dHelper.COLUMN_M_IMAGE, menu.getM_image());
+		values.put(dHelper.COLUMN_M_NAME, menu.getM_name());
+		values.put(dHelper.COLUMN_M_PRICE, menu.getM_price());
+		values.put(dHelper.COLUMN_M_STATUS, menu.getM_status());
+		
+		//insert row
+		int menuId = (int) db.insert(dHelper.TABLE_MENU, null, values);
+		return menuId;
 	}
 
 	@Override
 	public boolean updateMenu(Menu menu) {
-		// TODO Auto-generated method stub
-		return false;
+		SQLiteDatabase db = dHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		
+		values.put(dHelper.COLUMN_M_IMAGE, menu.getM_image());
+		values.put(dHelper.COLUMN_M_NAME, menu.getM_name());
+		values.put(dHelper.COLUMN_M_PRICE, menu.getM_price());
+		values.put(dHelper.COLUMN_M_STATUS, menu.getM_status());
+		
+		try{
+			db.update(dHelper.TABLE_MENU, values,
+					dHelper.COLUMN_M_ID + " = ? ",
+					new String[] { String.valueOf(menu.getM_food_id()) });
+			return true;
+		}catch(Exception e){
+			return false;
+		}
 	}
 
 	@Override
 	public boolean changeStatusFood(int foodId, boolean status) {
-		// TODO Auto-generated method stub
-		return false;
+		Menu menu = getFood(foodId);
+		if(menu == null)
+			return false;
+		menu.setM_status(status);
+		return this.updateMenu(menu);
 	}
 
 	@Override
 	public Menu getFood(int foodId) {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		String query = "SELECT * FROM " + dHelper.TABLE_MENU + "WHERE "
+				+ dHelper.COLUMN_M_ID + " = " + foodId;
+		Log.e(dHelper.LOG, query);
+		
+		Cursor c = db.rawQuery(query, null);
+		if(c != null)
+			c.moveToFirst();
+		
+		Menu menu = new Menu();
+		menu.setM_food_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_ID)));
+		menu.setM_image(c.getString(c.getColumnIndex(dHelper.COLUMN_M_IMAGE)));
+		menu.setM_name(c.getString(c.getColumnIndex(dHelper.COLUMN_M_NAME)));
+		menu.setM_price(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_PRICE)));
+		menu.setM_status(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_STATUS)) == 1);
+		
+		return menu;
 	}
 
 	@Override
 	public List<Menu> getAllFood() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Menu> menus = new ArrayList<Menu>();
+		String query = "SELECT * FROM " + dHelper.TABLE_MENU;
+		Log.e(dHelper.LOG,query);
+		
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		Cursor c = db.rawQuery(query, null);
+		
+		// looping through all rows and adding to list
+		if(c.moveToFirst()){
+			do{
+				Menu menu = new Menu();
+				menu.setM_food_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_ID)));
+				menu.setM_image(c.getString(c.getColumnIndex(dHelper.COLUMN_M_IMAGE)));
+				menu.setM_name(c.getString(c.getColumnIndex(dHelper.COLUMN_M_NAME)));
+				menu.setM_price(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_PRICE)));
+				menu.setM_status(c.getInt(c.getColumnIndex(dHelper.COLUMN_M_STATUS)) == 1);
+				
+				menus.add(menu);
+			}while(c.moveToNext());
+		}
+		return menus;
 	}
 
 	@Override
 	public int createBill(Bill bill) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
@@ -108,6 +295,29 @@ public class DatabaseSource implements SqliteAPIs{
 	public Bill getBill(int billId) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public List<Bill> getAllBill() {
+		List<Bill> bills = new ArrayList<Bill>();
+		
+		SQLiteDatabase db = dHelper.getReadableDatabase();
+		String query = "SELECT * FROM " + dHelper.TABLE_BILL;
+		Log.e(dHelper.LOG, query);
+		Cursor c = db.rawQuery(query, null);
+		
+		//looping through all rows and adding to list
+		if(c.moveToFirst()){
+			do{
+				Bill bill = new Bill();
+				bill.setB_id(c.getInt(c.getColumnIndex(dHelper.COLUMN_B_ID)));
+				bill.setB_count(c.getInt(c.getColumnIndex(dHelper.COLUMN_B_COUNT)));
+//				bill.setB_time_stamp(c.getString(c.getColumnIndexOrThrow(dHelper.COLUMN_B_TIME_STAMP)));
+				
+				bills.add(bill);
+			}while(c.moveToNext());
+		}
+		return bills;
 	}
 
 	@Override
